31882ee6b78d9a0c2fc89d6702df87784152715c
paulspartan14 <e17081416@itmerida.edu.mx>
2022-01-07 17:58:25 -0600

    added config to docker & docker-compose settings

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const { PORT = 3300 } = process.env
 
 // Start Server
 app.listen(PORT, () => {
-    console.log(`API running on http://localhost:${PORT}`)
+    console.log(`API running on http://localhost:${PORT}/api/`)
 })
